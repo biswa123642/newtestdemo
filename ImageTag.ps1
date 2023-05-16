4dc9bd5f041f077c6bd7f51ce4a20bbaf664b169
biswa123642 <64695542+biswa123642@users.noreply.github.com>
2023-05-16 22:10:21 +0530

    Update ImageTag.ps1

--- ImageTag.ps1
+++ ImageTag.ps1
@@ -1,4 +1,3 @@
 Get-ChildItem 'kustomization.yaml' -Recurse | ForEach {
-     (Get-Content $_ | ForEach  { $_ -replace '{{BUILD_NUMBER}}', '$BUILD_NUMBER' }) |
-     Set-Content $_
+     (Get-Content $_ | ForEach  { $_ -replace '{{BUILD_NUMBER}}', '$BUILD_NUMBER' })
 }
